c0390785aa3c38db7bb19ba2a264d345366e582f
42 <fortytwo@42s-iMac-5.local>
2019-11-11 14:58:33 -0800

    add spray.ino comment

--- paintbot/spray.ino
+++ paintbot/spray.ino
@@ -1,4 +1,17 @@
-///////////////////////////////////////////////////////////////// SPRAY ///////////////////////////////////////////////////////
+/************************************************************* SPRAY ***********************************************************
+ * Functions in this tab                                                                                                       *
+ * 1. Spray              : Tturns the z motor to engage the trigger to start spraying                                          *
+ * 2. stop_Spraying      : Turns the z motor to disengage the trigger to stop spraying                                         *
+ * 3. main_ZMotor        : Main logic for mode CALLIBRATE_ZMOTOR: press to spin Zmotor, release to stop spinning               *
+ * 4. spin_ZMotor        : Only used for mode CALLIBRATE_ZMOTOR: Determines the direction of spin of Zmotor (CW, CCW, or STOP) *
+ * 5. check_spray_status : Checks if the trigger is on or off and disengages the trigger                                       *
+ ******************************************************************************************************************************/
+
+/*
+ *  Changes the value in EEPROM of spray_Address to 1 (true for spraying)
+ *  Rotates the encoder/motor by set angle to engage the trigger and start spraying
+ */
+
 void spray() {
 
   int encZ = MiddleMotorEnc.read();
@@ -17,6 +30,11 @@ void spray() {
     Serial.print(","); 
 }
 
+/*
+ *  Changes the value in EEPROM of spray_Address to 0 (false for spraying)
+ *  Rotates the encoder/motor by set angle to disengage the trigger and stop spraying
+ */
+
 int stopSpraying() {
   int encZ = MiddleMotorEnc.read();
       
@@ -34,6 +52,14 @@ int stopSpraying() {
     return (1);
 }
 
+/*
+ *  Main logic of CALLIBRATE_ZMOTOR mode: 
+ *  If the switch is pressed, the Zmotor spins in the indicated direction until switch is released
+ *  @param click_status: input from digitalread to see if switch was clicked
+ *  @param spin_direction: CW for clockwise, or CCW for Counterclockwise
+ *  @param power: The ratio of power supplied to Zmotor to determine the speed of the spin
+ */
+ 
 void main_ZMotor(int click_status, int spin_direction, int power)
 {
     while (click_status == CLICKED) {
@@ -45,6 +71,12 @@ void main_ZMotor(int click_status, int spin_direction, int power)
       }    
     }
 }
+
+/*
+ *  Takes input and writes values to pins accordingly to spin clockwise, counterclockwise, or not at all
+ *  @param: turnDirection: Choices of CW, CCW, and Stop
+ *  @param: power: Ratio of pwer supplied to Zmotor to determine the speed of the spin
+ */
 
 void spinZMotor(int turnDirection, int power) {
    //turns in endless circle
@@ -59,6 +91,11 @@ void spinZMotor(int turnDirection, int power) {
        analogWrite(ZMOTOR_B_Output, 0);
    }
 }
+
+/*
+ *  function that checks the global variable of spray_status from EEPROM on Arduino
+ *  and executes the stopSpraying function if case is true for spraying
+ */
 
 void  check_spray_status()
 {
